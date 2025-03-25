1db909dbf38d982ce07990943547880d71cdc68e
Tony Le <tony@britwyn.co.nz>
2025-03-25 21:01:36 +1300

    refactor: index.js

--- src/index.js
+++ src/index.js
@@ -35,8 +35,9 @@ const main = async () => {
 	}
 };
 
-main().then((data) => {
+(async () => {
+	const data = await main();
 	console.log(`Prompt Tokens: ${data.usage.prompt_tokens}`);
 	console.log(`Completion Tokens: ${data.usage.completion_tokens}`);
 	console.log(`Total Tokens: ${data.usage.total_tokens}`);
-});
+})();
